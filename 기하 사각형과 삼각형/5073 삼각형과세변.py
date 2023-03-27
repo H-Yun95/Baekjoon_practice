a97a88c2dcb75b64c38d2d2e40fc11cadaa1394e
H-Yun95 <shxmqnr951@gmail.com>
2023-03-27 16:05:19 +0900

    230327_01

--- 기하 사각형과 삼각형/5073 삼각형과세변.py
+++ 기하 사각형과 삼각형/5073 삼각형과세변.py
@@ -10,32 +10,41 @@ Scalene : 세 변의 길이가 모두 다른 경우
 
 세 변의 길이가 주어질 때 위 정의에 따른 결과를 출력하시오.
 '''
+# 처음부터 다시
+# 0, 0, 1 = Invalid,
+# 0, 2, 1 = Invalid,
+# 2, 2, 8 = Invalid,
+# 6, 2, 6 = Isosceles 등이 나와야 함.
 
 
-def c(T):
-    a = max(T)
-    if sum(T) == 0:
-        return False
-    elif len(set(T)) == 1:
-        return 'Equilateral'
-    elif len(set(T)) == 2 and 0 not in T:  # 처음 돌려보니 실패를 해서
-        return 'Isosceles'       # 0, 0, 1 꼴에서도 Isosceles이 출력되는걸 발견,
-    else:                        # 그래서 고쳤는데도 또 틀렸다. 왜지?
-        T.remove(max(T))         # 반례 찾아보니 2, 2, 8 같은경우 이등변이 나온다.
-
-    if a >= sum(T):
-        return 'Invalid'
-    else:
-        return 'Scalene'
+def c(a):                       # 삼각형 판별 함수
+    if sum(a) == 0:             # 만약 총합이 0이면 = 0, 0, 0이면
+        return False            # False 리턴
+    if len(set(a)) == 1:        # set 원소가 1개면 = 3개 다 같으면
+        return 'Equilateral'    # 정삼각형
+    b = max(a)                  # 가장 큰 값 지정
+    a.remove(b)                 # '중요' 그 원소 단 하나만 빼기
+    if b >= sum(a):   # 0이 낑겨있거나 뺀 원소가 나머지보다 크거나 같다면 (0, 2, 2 의 경우 2 하나 빼도 어?)
+        return 'Invalid'        # 삼각형 안됨. 위에 0 in a 삭제해도 됨 ㅋㅋ
+    a.append(b)                 # 마무리 이등변 판별을 위해 뺀 원소 다시 더해줌. 순서 상관 없어서 append
+    if len(set(a)) == 2:        # 이제 set 원소가 2개면 = 2개만 같다면
+        return 'Isosceles'      # 이등변
+    return 'Scalene'            # 나머지는 걍 삼각형
 
 
 while 1:
     T = list(map(int, input().split()))
-    b = c(T)
-    if b:
-        print(b)
+    a = c(T)      # 이게 중요한데, 함수 결과값을 따로 변수로 지정 안하니까
+    if a:         # 루프가 이상하게 돌아감. 암튼 a가 True라면,
+        print(a)  # a 출력
     else:
         break
 
-# A = [0, 0, 0]
-# print(c(A))
+# 예외를 어떻게 처리하냐 로직 짜는걸걸로 짱구 좀 굴렸다....
+
+'''
+for z in open(0):
+    s=sorted(map(int,z.split()))
+    print(('Invalid','Equilateral','Isosceles','Scalene')[(s[2]<s[0]+s[1])*len({*s})]*(s[0]>0))
+숏코딩. 저 zip과 open은 알아둘 필요성이 보인다.
+'''

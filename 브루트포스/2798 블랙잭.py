790f1712f18e14afdc10929836d364c08c34f0d2
H-Yun95 <shxmqnr951@gmail.com>
2023-03-29 00:43:55 +0900

    Update 2798 블랙잭.py

--- 브루트포스/2798 블랙잭.py
+++ 브루트포스/2798 블랙잭.py
@@ -35,11 +35,53 @@ t = 0
 
 for x in range(n-2):
     for y in range(1, n-1):
-        for z in range(2, n):
-            s = a[x] + a[y] + a[z]
-            if s > t and s <= m:
-                t = s
+        if x == y:           # 밑의 문제를 해결하기 위해, x와 y가 다를 시 코드가 돌아가게 작동
+            continue         # 생각해보면 if x != y 하고 밑에 주르륵 썼으면
+        else:                # 더 간단하지 않았을까 싶음.
+            for z in range(2, n):
+                if z != x and z != y:       # 여기서도 z가 x, y와 다르다면 코드 실행
+                    s = a[x] + a[y] + a[z]  # 세 수를 전부 더해주고,
+                    if s > t and s <= m:    # 그 수가 이전 합계보다 크면서 m 이하라면
+                        t = s               # 더한 수를 t로 정의
 
 print(t)
+
 # ! 이렇게 단순 반복으로만 짜면 동일한 수 ex) x=y=1일때 등
 # 가 더해질 수 있어서 값이 이상하게 나옴. 그걸 해결해야 함.
+
+'''
+밑에는 시간이 굉장히 짧게 걸려서 가져와본 녀석
+다른 짧은 코드들은 모듈을 잘 갖고 오는데, 이녀석은그런 것도 없이 굉장히 짧게 끝남.
+
+N, M = map(int,input().split())
+cards = list(map(int,input().split()))
+maxv = 0
+
+def myfunc():              어쩌면 함수 선언이 짧은 비결일 수도?
+    global maxv            C와 비슷하게? 함수 밖 변수에 영향을 주는 것으로 보임
+    for i in range(N):     아니다 싸피에서 배웠던 것 같은데
+        temp_maxv = cards[i]
+        if temp_maxv > M:
+            continue
+        for j in range(i+1, N):
+            temp_maxv = cards[i] + cards[j]
+            if temp_maxv > M:
+                continue
+            for k in range(j+1, N):
+                temp_maxv = cards[i] + cards[j] + cards[k]
+                if temp_maxv == M:
+                    maxv = temp_maxv
+                    return
+                if maxv < temp_maxv < M:
+                    maxv = temp_maxv  # 갱신
+                    # print(maxv)
+    return
+
+myfunc()
+print(maxv)
+
+ㅇㅎ. 총 n번 반복하고, 반복을 겹칠 수록 그때 카드를 더해서
+값이 m과 같다면 바로 리턴, 아니라면 밑으로 마저 연산을 해서
+이전값과 비교하여 다시 갱신하는 프로세스. 근데 이게 왜 이렇게
+짧게 걸릴까?
+'''
